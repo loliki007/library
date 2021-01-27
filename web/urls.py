fd7996538dc35cca0bbe429b65e190d7d8d941b6
Leila <lolikibeishembieva@gmail.com>
2021-01-28 00:32:15 +0600

    download template

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from . import views 
+
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+   
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,10 @@
+
 from django.shortcuts import render
+from django.urls import path
+
 
 def index(request):
     return render(request, 'web/index.html')
+
+
+
